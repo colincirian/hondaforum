7be56f36f37378c55dd92fbec4a81ee14edf72f5
Colin <colindcirian@gmail.com>
2023-09-17 21:31:13 -0700

    still working on registration, IM STRESSED THERES NOT ENOUGH TIME

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,64 +1,148 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import { useHistory } from 'react-router-dom';
-// import './Register.css';
-// import { supabase } from '../config/SupabaseClient';
-// import { Link } from 'react-router-dom'; // Import Link
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import "./Register.css";
+import { supabase } from "../config/SupabaseClient";
+import { Link } from "react-router-dom"; // Import Link
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSomeIcon, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
-// async function registerClient() {
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3, 23}$/;
+const PASS_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8, 24}$/;
+
+function Register() {
+
+  const userRef = useRef();
+  const errRef = useRef();
+
+  // Validate username
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  // Validate password
+  const [pass, setPass] = useState('');
+  const [validPass, setValidPass] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  // Validate that the passwords match
+  const [matchPass, setMatchPass] = useState('');
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  // Display either an error message or successful message
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+//   useEffect(() => {
+//     userRef.current.focus();
+//   }, [])
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = PASS_REGEX.test(pass);
+    console.log(result);
+    console.log(pass);
+    setValidPass(result);
+    const match = pass === matchPass;
+    setValidMatch(match);
+  }, [pass, matchPass]);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pass, matchPass])
+
+  return (
+    <section>
+        <p ref={errRef} className={errMsg ? "errmsg" :
+        "offscreen"} aria-live="assertive">{errMsg}</p>
+        <h1>Register</h1>
+        <form>
+            <label htmlFor="username">
+                Username:
+            </label>
+            <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                required
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+            />
+            <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                <FontAwesomeIcon icon={faInfoCircle} />
+                4 - 24 characters.<br />
+                Must begin with a letter.<br />
+                Letters, numbers, underscores, hyphens allowed.
+            </p>
+        </form>
+    </section>
+  )
+}
+
+export default Register;
 
 //   const { client } = auth.currentClient; // Get the authenticated user
 //   const [password, setPassword] = useState('');
 //   const history = useHistory();
 
-//   // try {
-//   //   // Create a new client record
-//   //   const { data, error } = await supabase
-//   //     .from('clients')
-//   //     .select('*')
-//   //     .upsert([
-//   //       {
-//   //         user_id: user.id, // Link to the user
-//   //         name: "Client Name",
-//   //         email: "Client@gmail.com",
-//   //       },
-//   //     ], { onConflict: ['email'] });
+//   try {
+//     // Create a new client record
+//     const { data, error } = await supabase
+//       .from('clients')
+//       .select('*')
+//       .upsert([
+//         {
+//           user_id: user.id, // Link to the user
+//           name: "Client Name",
+//           email: "Client@gmail.com",
+//         },
+//       ], { onConflict: ['email'] });
 
-//   //     if (error) {
-//   //       // Display an error message to the user 
-//   //       console.error('Error registering client:', error.message);
-//   //     } else {
-//   //       // Registration for the client was successful
-//   //       console.log('Client registration successful:', data);
-//   //       history.push("/login");
-//   //     }
-//   // } catch (e) {
-//   //   console.error('An unexcepected error occurred:', e);
-//   // }
+//       if (error) {
+//         // Display an error message to the user
+//         console.error('Error registering client:', error.message);
+//       } else {
+//         // Registration for the client was successful
+//         console.log('Client registration successful:', data);
+//         history.push("/login");
+//       }
+//   } catch (e) {
+//     console.error('An unexcepected error occurred:', e);
+//   }
 
-//   // const handleRegistration = async () => {
-//   //   try {
-//   //     // Step 1: Create a user account using Supabase authentication
-//   //     const { user, error } = await supabase.auth.signUp({ email: username, password });
+//   const handleRegistration = async () => {
+//     try {
+//       // Step 1: Create a user account using Supabase authentication
+//       const { user, error } = await supabase.auth.signUp({ email: username, password });
 
-//   //     if (error) {
-//   //       console.error('Registration failed:', error.message);
-//   //       // Handle registration error (e.g., display an error message)
-//   //     } else {
-//   //       // Registration successful, user is now created
-//   //       console.log('User registered:', user);
+//       if (error) {
+//         console.error('Registration failed:', error.message);
+//         // Handle registration error (e.g., display an error message)
+//       } else {
+//         // Registration successful, user is now created
+//         console.log('User registered:', user);
 
-//   //       // Step 2: Navigate to another page (login) for additional user setup
-//   //       history.push('/login');
-//   //     }
-//   //   } catch (error) {
-//   //     console.error(error);
-//   //     // Handle unexpected errors
-//   //   }
-//   // };
+//         // Step 2: Navigate to another page (login) for additional user setup
+//         history.push('/login');
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       // Handle unexpected errors
+//     }
+//   };
 
-//   return (
-//     <div className="registration-container">
+//  <div className="registration-container">
 //       <h2>Register</h2>
 //       <form className="registration-form">
 //         <input
@@ -85,7 +169,4 @@
 //         </Link>
 //       </form>
 //     </div>
-//   );
-// }
 
-// export default registerClient;
--- src/components/UserPost.js
+++ src/components/UserPost.js
@@ -1,10 +1,16 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import "./UserPost.css";
 import { supabase } from "../config/SupabaseClient";
+import { click } from "@testing-library/user-event/dist/click";
 
 function UserPost() {
+
 // Handle form submission
 const handleSubmit = async (event) => {
+
   event.preventDefault();
   const username = event.target.username.value;
   const comment = event.target.comment.value;
@@ -26,10 +32,10 @@ const handleSubmit = async (event) => {
       <h1>Create a Post!</h1>
       <div className="post-container">
         <section className="user-textbox">
-          <form onSubmit={handleSubmit}>
+          <form>
             <input placeholder="Username" type="text" id="username" />
             <input placeholder="Comment" type="text" id="comment" />
-            <input type="submit" value="Post" />
+            <button type="submit">Submit Post</button>
           </form>
         </section>
       </div>
